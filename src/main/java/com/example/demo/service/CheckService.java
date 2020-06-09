5616e0673149a2235113ec500d19b79c47f98080
Suyambu <60639657+Suyambu@users.noreply.github.com>
2020-06-09 19:03:30 +0530

    Update CheckService.java

--- src/main/java/com/example/demo/service/CheckService.java
+++ src/main/java/com/example/demo/service/CheckService.java
@@ -115,6 +115,7 @@ public class CheckService {
 					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
 			con.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
 			con.addRequestProperty("Referer", "https://www.google.com/");
+			con.setRequestMethod("HEAD");
 			con.setReadTimeout(2500);
 			con.connect();
 			// con.getInputStream();
