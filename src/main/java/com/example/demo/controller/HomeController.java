ef97e3a38cf043c70d1064241792b50b9de74923
Suyambu <60639657+Suyambu@users.noreply.github.com>
2020-06-09 16:28:44 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,9 +2,14 @@ package com.example.demo.controller;
 
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,16 +32,36 @@ public class HomeController {
 	}
 		
 	@RequestMapping("/xlForm")
-	public ModelAndView submitXlForm(CheckCondition checkCond,MultipartFile mfile) throws IOException {
+	public void submitXlForm(HttpServletResponse response,CheckCondition checkCond,MultipartFile mfile) throws IOException {
 		
 		CheckService service = new CheckService();
 		 File convFile = new File( mfile.getOriginalFilename() );
 	        FileOutputStream fos = new FileOutputStream( convFile );
 	        fos.write( mfile.getBytes() );
 	        fos.close();
-		service.checkUrl(convFile,checkCond);
+		File f = service.checkUrl(convFile,checkCond);
+		if(f != null) {
+			
+		
 		ModelAndView mv = new ModelAndView("index");
-		return mv;
+		  InputStream inputStream = new FileInputStream(f);
+	        response.setContentType("application/force-download");
+	        String fileName = "updated";
+	        response.setHeader("Content-Disposition", "attachment; filename="+fileName+".xlsx"); 
+	        IOUtils.copy(inputStream, response.getOutputStream());
+	        response.flushBuffer();
+	        inputStream.close();
+	        if (f.delete()) { 
+	            System.out.println("Deleted the file: " + f.getName());
+	          } else {
+	            System.out.println("Failed to delete the file.");
+	          } 
+	    
+		}else {
+			
+			response.sendRedirect("index");
+			
+		}
 		
 	}
 	
